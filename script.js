1e1184274a7ccc69cf3b94abf092b20a591dc232
Bronx715 <brondonemaeva@gmail.com>
2024-10-11 19:48:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Bienvenue sur ClimateCare & SSR - Soyez prêts pour les alertes climatiques et les conseils de santé.');
+});
